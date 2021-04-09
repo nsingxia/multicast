25dbe3e892d30e75dfb2369e0a9249e92555af65
nsxia <nsxia@sse.com.cn>
2021-04-09 18:37:51 +0800

    a

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
 #include "opt.h"
 #include "multicast.h"
 
+#include <signal.h>
+
 void help();
+
+void procExit(int signo);
 
 int main(int argc,char **argv)
 {
-    std::string m;
+    signal(SIGINT,procExit);
+    signal(SIGTERM,procExit);
+
+    std::string m = "send";
+    std::string addr = "224.1.0.104:1235,127.0.0.1";
+
     Option opt(argc,argv,help);
     opt.addOpt(m,"m:");
+    opt.addOpt(addr,"a:");
     opt.init();
+
+    std::cout<< "method:" << m<<std::endl;
+    std::cout<< "addr:" << addr <<std::endl;
+
     return 0;
 }
 
@@ -16,4 +30,19 @@ int main(int argc,char **argv)
 void help()
 {
     std::cout<<"help"<<std::endl;
+}
+
+void procExit(int signo)
+{
+    std::cout<<"multicast recv signal:"<<signo<<" to exit"<<std::endl;
+}
+
+void send()
+{
+
+}
+
+void recv()
+{
+
 }
