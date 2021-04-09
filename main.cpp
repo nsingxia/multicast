4d2fa61e625a01962ec91eff026a469dc0cacff5
nsxia <nsxia@sse.com.cn>
2021-04-09 18:29:58 +0800

    a

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include "opt.h"
 #include "multicast.h"
 
+void help();
+
 int main(int argc,char **argv)
 {
-    Option opt(argc,argv);
+    std::string m;
+    Option opt(argc,argv,help);
+    opt.addOpt(m,"m:");
+    opt.init();
     return 0;
+}
+
+
+void help()
+{
+    std::cout<<"help"<<std::endl;
 }
